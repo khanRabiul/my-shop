b54c857cb4deca7305cb3d14cb4da277cda7e881
KhanRabiul <krabiulislam2@gmail.com>
2025-05-26 22:53:22 +0600

    [TopNavSocialLink] styled with media home page link for landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import TopNavbar from "@/ui/landingPage/header/TopNavbar";
+import TopNavSocialLink from "@/ui/landingPage/header/TopNavSocialLink";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <TopNavbar />
+        <TopNavSocialLink />
         <main>
           {children}
         </main>
---
+++ src/ui/landingPage/header/TopNavSocialLink.tsx
@@ -0,0 +1,59 @@
+import { X } from 'lucide-react';
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa6";
+import { FaPinterest } from "react-icons/fa";
+import { FaReddit } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaYoutube } from "react-icons/fa";
+import Link from "next/link";
+
+const TopNavSocialLink = () => {
+  return (
+    <section className="bg-[#1B6392] py-1.5">
+      <div className="container">
+        <div
+          className="flex items-center justify-between">
+          <div>
+            <p className="text-white text-sm">Welcome to My Shop Multivendor Ecommerce website</p>
+          </div>
+          
+          <div className='inline-flex items-center gap-3 text-white'>
+            <span>Follow us:</span>
+            <Link 
+            className='cursor-pointer text-lg'
+             href='https://x.com/'>
+              <FaXTwitter />
+            </Link>
+            <Link 
+            className='cursor-pointer text-lg'
+             href='https://facebook.com/'>
+              <FaFacebook />
+            </Link>
+            <Link 
+            className='cursor-pointer text-lg'
+             href='https://pinterst.com/'>
+              <FaPinterest />
+            </Link>
+            <Link 
+            className='cursor-pointer text-lg'
+             href='https://pinterst.com/'>
+              <FaReddit />
+            </Link>
+            <Link 
+            className='cursor-pointer text-lg'
+             href='https://pinterst.com/'>
+              <FaYoutube />
+            </Link>
+            <Link 
+            className='cursor-pointer text-lg'
+             href='https://pinterst.com/'>
+              <FaInstagram />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default TopNavSocialLink;
