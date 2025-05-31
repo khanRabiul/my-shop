c7c26b4fb04e0f63b3f07c24887a1d7aefa0cf84
KhanRabiul <krabiulislam2@gmail.com>
2025-05-31 13:41:20 +0600

    [TrackOrder.tsx] menu button

--- src/ui/landingPage/header/TopMenu.tsx
+++ src/ui/landingPage/header/TopMenu.tsx
@@ -1,10 +1,12 @@
 import AllCategory from "@/ui/landingPage/header/AllCategory";
+import TrackOrder from "@/ui/landingPage/header/TrackOrder";
 
 const TopMenu = () => {
   return (
-    <nav>
+     <nav className="bg-background text-foreground py-2">
       <div className="container flex items-center space-x-4">
         <AllCategory />
+        <TrackOrder />
       </div>
     </nav>
   );
